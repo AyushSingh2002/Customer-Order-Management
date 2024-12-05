995ba622a6bc24ddd78fe948daf14a57645e3a49
Ayush Singh <ayush.s@incture.com>
2024-12-05 10:22:09 +0530

    Initail Commit

---
+++ src/main/java/com/incture/CustomerOrderManagement/Entities/Customer.java
@@ -0,0 +1,140 @@
+package com.incture.CustomerOrderManagement.Entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table
+public class Customer 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long customerId;
+	
+	@Column
+	private String customerName;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String phoneNumber;
+
+	@Column
+	private String address;
+	
+	@Column
+	private String city;
+	
+	@Column
+	private Integer pinCode;
+	
+	@Column
+	private String state;
+	
+	@Column
+	private String country;
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Integer getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(Integer pinCode) {
+		this.pinCode = pinCode;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Customer(Long customerId, String customerName, String email, String phoneNumber, String address, String city,
+			Integer pinCode, String state, String country) {
+		super();
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.city = city;
+		this.pinCode = pinCode;
+		this.state = state;
+		this.country = country;
+	}
+
+	public Customer() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", customerName=" + customerName + ", email=" + email
+				+ ", phoneNumber=" + phoneNumber + ", address=" + address + ", city=" + city + ", pinCode=" + pinCode
+				+ ", state=" + state + ", country=" + country + "]";
+	}
+	
+}
